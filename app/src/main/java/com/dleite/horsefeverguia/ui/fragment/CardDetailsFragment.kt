9977cf968e440a0c96d6dacc3cbb0a9548e482e2
C1320761 Danilo Fernando Cavalcanti Leite <c1320761@interno.bb.com.br>
2023-05-26 18:00:24 -0300

    Criação da tela de detalhe

---
+++ app/src/main/java/com/dleite/horsefeverguia/ui/fragment/CardDetailsFragment.kt
@@ -0,0 +1,35 @@
+package com.dleite.horsefeverguia.ui.fragment
+
+import com.dleite.horsefeverguia.R
+import com.dleite.horsefeverguia.databinding.FragmentDetailsCardBinding
+import com.dleite.horsefeverguia.ui.adapter.CardAdapter
+import com.dleite.horsefeverguia.ui.core.viewBinding
+import com.dleite.horsefeverguia.ui.viewmodel.CardDatailsViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class CardDetailsFragment : BaseFragment(R.layout.fragment_details_card) {
+
+    private val binding by viewBinding (FragmentDetailsCardBinding::bind)
+
+    private val viewModel: CardDatailsViewModel by viewModel()
+
+    private val cardAdapter = CardAdapter()
+
+    override fun baseInitEvents() {
+        super.baseInitEvents()
+        setupListeners()
+    }
+
+    override fun baseInitObservers() {
+        super.baseInitObservers()
+     //   viewModel.cardsData.observe(viewLifecycleOwner) { cards ->
+     //       cardAdapter.submitList(cards)
+     //   }
+    }
+    private fun setupListeners(){
+       // viewModel.getCards()
+    }
+
+
+
+}
---
+++ app/src/main/java/com/dleite/horsefeverguia/ui/viewmodel/CardDatailsViewModel.kt
@@ -0,0 +1,12 @@
+package com.dleite.horsefeverguia.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.dleite.horsefeverguia.domain.usercase.card.GetCardUserCase
+
+class CardDatailsViewModel(
+    private val getCardUserCase: GetCardUserCase
+) : ViewModel() {
+
+
+
+}
